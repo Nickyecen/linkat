335efec4abb361e3a4a6fffd9724d9638507e14f
Nickyecen <nicolasecendron@gmail.com>
2026-02-12 11:32:40 -0300

    Clear extra files
    Signed-off-by: Nickyecen <nicolasecendron@gmail.com>

--- link.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <string>
-
-std::string link(const std::string &fileName, const std::string &header,
-                 const std::string &tail, const bool recursive);
--- read_input.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <string>
-
-void readInput(const int argc, char *const argv[], std::string &fileName,
-               std::string &header, std::string &tail, bool &recursive);
